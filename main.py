c0ee23390a3bca88eebe1bc005f582110b972797
Никита <nikitakostrov96@gmail.com>
2023-06-30 17:47:28 +0300

    добавил новую функцию create_file_with_name

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import os
 def get_path ():
     "Функция получения адресса исполняемого файла"
     return os.getcwd()
+def create_file_with_name():
+    # Открыть файл для записи
+    with open(test, 'w') as file:
+        # Записать имя файла внутри файла
+        file.write(file_name)
 
+# Пример использования функции
 
 if __name__ == "__main__":
     print(get_path())
