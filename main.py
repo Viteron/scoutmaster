ce75c56e679618d378f72a26595f8c5351591ac4
Viktor Kostrov <Viktor.kostrov92@yandex.ru>
2023-06-30 18:55:41 +0300

    2 функции с os

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 
-def get_path ():
+def get_path():
     "Функция получения адресса исполняемого файла"
     return os.getcwd()
 def create_file_with_name():
@@ -11,6 +11,17 @@ def create_file_with_name():
         file.write(file_name)
 
 
+def take_list_dirict():
+    "Функция получения списка папок"
+    return os.listdir()
+
+
+def chenge_path_in_scout():
+    "Функция изменения пути к видео в скаутах"
+    pass
+
+
 if __name__ == "__main__":
-    print(get_path())
+    #print(get_path())
+    take_list_dirict()
     input("все готово!")
