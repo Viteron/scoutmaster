5162e7a00f8561e233e674db1d93c28627fa0d23
Никита <nikitakostrov96@gmail.com>
2023-06-30 18:00:57 +0300

    добавил функцию create_file_with_name

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def create_file_with_name():
         # Записать имя файла внутри файла
         file.write(file_name)
 
-# Пример использования функции
 
 if __name__ == "__main__":
     print(get_path())
